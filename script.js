6bdf7c8306092b3795debbb48d485fe374f3b99e
Zane [Estere] Gruntmane <zaneestere@gmail.com>
2025-01-19 22:24:24 +0000

    updated code with media player work in progress

---
+++ script.js
@@ -0,0 +1,24 @@
+const progress = document.getElementById ("progress");
+const song = document.getElementById ("song");
+const ctrlIcon = document.getElementById ("ctrlIcon");
+
+song.onloadedmetadata = () => {
+    progress.max = song.duration; 
+    progress.value = song.currentTime; 
+}
+
+const playPause = () => {
+
+    if (ctrlIcon.classList.contains("fa-pause")) {
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    }
+    else {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+}
+
+ctrlIcon.addEventListener("click", playPause);
